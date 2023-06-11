56db84fe9771495391f85554b5af7c7e424739a6
minngaeng <minngaeng@gmail.com>
2023-06-12 02:46:41 +0900

    section 04

--- class/pages/_app.js
+++ class/pages/_app.js
@@ -1,6 +1,6 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-function MyApp({ Component }) {
+export default function App({ Component }) {
   const client = new ApolloClient({
     uri: 'http://backend-example.codebootcamp.co.kr/graphql',
     cache: new InMemoryCache(),
---
+++ class/pages/section04/04-03-graphql-mutation-args/index.js
@@ -0,0 +1,32 @@
+import { useMutation, gql } from '@apollo/client';
+
+//createBoard(writer: "철수", title: "안녕핫요", contents: "반갑습니다") {
+const 나의그래프큐엘셋팅 = gql`
+  mutation createBoard($writer: String, $title: String, $contents: String) {
+    createBoard(writer: $writer, title: $title, contents: $contents) {
+      _id
+      number
+      message
+    }
+  }
+`;
+// 훈이 , 안녕하세요, 반갑습니다. 
+
+
+export default function GraphqlMutationPage() {
+  const [나의함수] = useMutation(나의그래프큐엘셋팅);
+
+  const onClickSubmit = async () => {
+    const result = await 나의함수({
+      variables : {  //    => variables 이게 $ 역할을 함 
+        writer: '훈이',
+        title: '안녕하세요!',
+        contents: '반갑습니다',
+      },
+    });
+    console.log(result);
+  };
+
+  return <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>;
+}
+
