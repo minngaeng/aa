27a82f1e2cd10cec90406068eeaa5dad5934dd7f
minngaeng <minngaeng@gmail.com>
2023-06-19 15:25:13 +0900

    ADD:board UI

--- freeboard/pages/boards/index.js
+++ freeboard/pages/boards/index.js
@@ -1,24 +1,84 @@
+import { Input } from 'antd';
 import {
   Wrapper,
   Title,
   InputWrapper,
   Label,
   WriterInput,
+  WriterPwdContainer,
+  TitleInput,
+  ContentsInput,
+  Zipcode,
+  SearchBtn,
+  ZipcodeWrapper,
+  Address,
+  Youtube,
+  UploadButton,
+  UploadWrapper,
 } from '../../styles/board.style';
 
 export default function Board() {
   return (
     <Wrapper>
       <Title>게시물 수정 </Title>
-      <div>
+      <WriterPwdContainer>
         <InputWrapper>
           <Label>작성자</Label>
           <WriterInput type="text" placeholder="이름을 적어주세요." />
         </InputWrapper>
-        {/* <div>
-          비밀번호
-          <input />
-        </div> */}
+        <InputWrapper>
+          <Label>비밀번호</Label>
+          <WriterInput type="text" placeholder="비밀번호를 입력해주세요." />
+        </InputWrapper>
+      </WriterPwdContainer>
+      <InputWrapper>
+        <Label>제목</Label>
+        <TitleInput type="text" placeholder="제목을 작성해주세요." />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>내용</Label>
+        <ContentsInput type="text" placeholder="내용을 작성해주세요." />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>주소</Label>
+        <ZipcodeWrapper>
+          <Zipcode placeholder="07250" />
+          <SearchBtn>우편번호 검색</SearchBtn>
+        </ZipcodeWrapper>
+        <Address />
+        <Address />
+      </InputWrapper>
+      <InputWrapper>
+        <Label>유튜브</Label>
+        <Youtube />
+      </InputWrapper>
+      <div>
+        <Label>사진 첨부</Label>
+        <UploadWrapper>
+          <UploadButton>
+            <div>+</div>
+            <div>Upload</div>
+          </UploadButton>
+          <UploadButton>
+            <div>+</div>
+            <div>Upload</div>
+          </UploadButton>
+          <UploadButton>
+            <div>+</div>
+            <div>Upload</div>
+          </UploadButton>
+        </UploadWrapper>
+      </div>
+      <div>
+        <Label>메인 설정</Label>
+        <input type="radio" />
+        <label>유튜브</label>
+        <input type="radio" />
+        <label>사진</label>
+      </div>
+      <div>
+        <button>취소하기</button>
+        <button>수정하기</button>
       </div>
     </Wrapper>
   );
--- freeboard/styles/board.style.js
+++ freeboard/styles/board.style.js
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   width: 1200px;
   height: 1847px;
   font-weight: 500;
@@ -16,8 +19,9 @@ export const Title = styled.h1`
 `;
 
 export const InputWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  padding-right: 24px;
+  padding-bottom: 40px;
+  /* margin-left: 102px; */
 `;
 
 export const Label = styled.div`
@@ -30,4 +34,79 @@ export const WriterInput = styled.input`
   border: 1px solid #bdbdbd;
   width: 486px;
   height: 52px;
+  ::placeholder {
+    padding: 16px;
+  }
+`;
+
+export const WriterPwdContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const TitleInput = styled.input`
+  width: 996px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+`;
+
+export const ContentsInput = styled.input`
+  width: 996px;
+  height: 480px;
+  border: 1px solid #bdbdbd;
+  padding: 14px;
+  padding-left: 16px;
+`;
+
+export const Zipcode = styled.input`
+  width: 77px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  /* padding-left: 16px; */
+  /* padding: 14px; */
+  text-align: center;
+`;
+
+export const SearchBtn = styled.button`
+  width: 124px;
+  height: 52px;
+  margin-left: 16px;
+  background-color: black;
+  cursor: pointer;
+  color: white;
+`;
+
+export const ZipcodeWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Address = styled.input`
+  width: 996px;
+  height: 52px;
+  border: 1px solid #bdbdbd;
+  margin-top: 16px;
+`;
+
+export const Youtube = styled.input`
+  width: 996px;
+  height: 52px;
+`;
+
+export const UploadWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+export const UploadButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-right: 24px;
+  background: #bdbdbd;
+  width: 78px;
+  height: 78px;
+  border: 1px solid #bdbdbd;
 `;
